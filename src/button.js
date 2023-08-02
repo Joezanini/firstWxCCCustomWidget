d4493af0cb30326498ba2a442df8e97c18ae7f36
jozanini <jozanini@cisco.com>
2023-08-02 16:37:09 -0700

    added files

---
+++ src/button.js
@@ -0,0 +1,90 @@
+const buttonTemplate = document.createElement("template");
+buttonTemplate.innerHTML = /* html */`
+        <style>
+            :host {
+                display: block;
+            }
+            :host([inprogress]) {
+                transform: scale(1.1);
+                transform-origin: top left;
+            }
+            .btn{
+                background-color:rgb(0, 145, 255);
+                color: white;
+                border: none;
+                border-radius: 15px;
+                padding: 0 2rem;
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                box-shadow: 0 4px 14px 0 rgb(0, 145, 255);
+            }
+
+            .btn:hover {
+                background-color: rgb(0, 174, 255);
+            }
+
+            .btn:disabled {
+                background-color: rgb(0, 251, 255);
+            }
+
+            .fading {
+                animation: fading 0.5s infinite
+            }
+
+            @keyframes fading {
+                0% {
+                    color: #6aa8f0;
+                }
+                50% {
+                    color: white;
+                }
+                100% {
+                    color: #6aa8f0;
+                }
+            }
+        </style>
+        <button class="btn"><slot>Button T</slot></button>
+`
+
+class Button extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback() {
+        this.shadowRoot.appendChild(buttonTemplate.content.cloneNode(true));
+        this.button = this.shadowRoot.querySelector("button");
+        this.initialValue = this.innerHTML;
+    }
+
+    set inprogress(progress) {
+        if (progress) {
+            this.setAttribute("inprogress", "true");
+        } else {
+            this.removeAttribute("inprogress");
+        }
+    }
+
+    get inprogress() {
+        return this.getAttribute("inprogress");
+    }
+
+    static get observedAttributes() {
+        return ["inprogress"]
+    }
+
+    attributeChangedCallback(attribute, oldValue, newValue) {
+        if (newValue) {
+            this.innerHTML = "Loading...";
+            this.button.setAttribute("disabled", "true");
+            this.button.classList.add('fading');
+        } else {
+            this.innerHTML = this.initialValue;
+            this.button.removeAttribute("disabled");
+            this.button.classList.remove('fading');
+        }
+    }
+}
+
+customElements.define("app-button", Button);
+
---
+++ src/input.js
@@ -0,0 +1,44 @@
+const template = document.createElement("template");
+template.innerHTML = /* html */ `
+<style>
+    label {
+        display: block;
+    }
+    input {
+        min-width: 200px;
+        border-radius: 3px;
+        border: 1px solid lightgray;
+        padding: 10px;
+    }
+</style>
+<label></label>
+<input>
+`;
+
+class Input extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    set value(value) {
+        this.setAttribute("value", value);
+    }
+
+    connectedCallback() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        const label = this.shadowRoot.querySelector("label");
+        label.textContent = this.getAttribute("label");
+        const input = this.shadowRoot.querySelector("input");
+        input.addEventListener("input", (event) => {
+            event.stopPropagation();
+            input.dispatchEvent(new CustomEvent('app-input', {
+                bubbles: true,
+                composed: true,
+                detail: event.target.value
+            }));
+        });
+    }
+}
+
+customElements.define("app-input", Input);
